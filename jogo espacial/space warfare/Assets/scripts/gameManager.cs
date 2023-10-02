3c1fdd836bf57b611c43fe43196b4d8dd0c24662
skarlate <penelopeskarlate@gmail.com>
2023-10-02 10:13:50 -0300

    update0210

--- jogo espacial/space warfare/Assets/scripts/gameManager.cs
+++ jogo espacial/space warfare/Assets/scripts/gameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
@@ -10,16 +11,33 @@ public class gameManager : MonoBehaviour
     public int pontuaçãoAtual;
 
     public Text pontuação;
+
+    [SerializeField] private string lvl_1;
+    
     // Start is called before the first frame update
 
     private void Awake()
     {
-        instance = this;
+        if (instance==null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void CarregarCenas()
+    {
+        SceneManager.LoadScene("GUI");
+        SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Additive);
     }
 
     void Start()
     {
-        
+        SceneManager.LoadScene("lvl_1");
     }
 
     // Update is called once per frame
@@ -32,5 +50,11 @@ public class gameManager : MonoBehaviour
     {
         pontuação.text = "PONTUAÇAÕ:" + pontuaçãoAtual;
         pontuaçãoAtual += pontosParaGanhar;
+    }
+    
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("GUI");
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
     }
 }
---
+++ jogo espacial/space warfare/Assets/scripts/iniciar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class iniciar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jogar()
+    {
+      //  SceneManager.LoadScene("SampleScene");
+        gameManager.instance.CarregarCenas();
+    }
+}
